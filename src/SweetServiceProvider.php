ec17435500dbdd4f7d3e865b93c9c12da339256e
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2017-01-23 07:30:06 +0000

    Add bootstrap sweet service

--- src/SweetServiceProvider.php
+++ src/SweetServiceProvider.php
@@ -7,6 +7,26 @@ use Illuminate\Support\ServiceProvider;
 class SweetServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap sweet service provider
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/views', 'sweet');
+
+        $this->publishes([
+            __DIR__ . '/views/sweet_message.blade.php' => base_path('resources/views/vendor/sweet/sweet_message.blade.php')
+        ], 'sweet_views');
+
+        $this->publishes([
+            __DIR__ . '/assets/sweetalert.css' => base_path('public/css/libs/sweetalert.css'),
+            __DIR__ . '/assets/sweetalert.js'  => base_path('public/js/libs/sweetalert.js')
+
+        ], 'sweet_assets');
+    }
+
+    /**
      * Register sweet service provider
      *
      * @return void
