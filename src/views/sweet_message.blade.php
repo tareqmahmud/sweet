ecdb2b9963f830748dcd8bc4813690edc4f04989
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2017-01-23 07:04:37 +0000

    Add sweet views

---
+++ src/views/sweet_message.blade.php
@@ -0,0 +1,15 @@
+<script>
+    swal({
+        title: "{{ session()->get("sweet_message.title") }}",
+        text: "{{ session()->get("sweet_message.message") }}",
+        type: "{{ session()->get("sweet_message.label") }}",
+        @if(session('sweet_message.autoHide'))
+            timer: "{{ session()->get('sweet_message.timer') }}",
+        @endif
+        @if(session('sweet_message.options'))
+            @foreach(session()->get('sweet_message.options') as $key => $value)
+                {{ $key }}: "{{ $value }}",
+            @endforeach
+        @endif
+    });
+</script>
